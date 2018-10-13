13ef4af052155dc30787f764896a835292d67ac9
ayushadi <43966297+ayushadi@users.noreply.github.com>
2018-10-13 23:09:11 +0530

    Create sumofadigitofano.c

---
+++ sumofadigitofano.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,temp,sum=0;
+int c=0;
+printf("enter no");
+scanf("%d",&n);
+while(n!=0)
+{
+temp=n%10;
+sum=sum*10+temp;
+n=n/10;
+c++;
+}
+printf("%d",c);
+}
