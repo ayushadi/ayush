d3939a2b35af2382c80da9a3bb6364520db43fe7
ayushadi <43966297+ayushadi@users.noreply.github.com>
2018-10-21 22:33:15 +0530

    Create arraysorting.c

---
+++ arraysorting.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    int a[100000],i,j,c,n;
+    printf("enter no");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        
+    }
+    for(i=0;i<n;i++)
+     {
+         for(j=i+1;j<n;j++)
+         {
+             if(a[i]>a[j])
+             {
+                 c=a[i];
+                 a[i]=a[j];
+                 a[j]=c;
+                 
+             }
+         }
+     }
+     for(i=0;i<n;i++)
+     {
+         printf("%d",a[i]);
+     }
+}
